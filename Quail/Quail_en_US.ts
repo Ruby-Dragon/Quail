b6a3ce964436aaa9a1564d075609749cd4ca02c2
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2023-01-26 08:59:50 -0800

    add UI

--- Quail/Quail_en_US.ts
+++ Quail/Quail_en_US.ts
@@ -1,3 +1,61 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"></TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="142"/>
+        <source>Quail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="42"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="143"/>
+        <source>Commands:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="49"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="144"/>
+        <source>Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="145"/>
+        <source>Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="146"/>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="147"/>
+        <source>IP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="77"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="148"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="149"/>
+        <source>Exit Node</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <location filename="cmake-build-debug/Quail_autogen/include/ui_mainwindow.h" line="150"/>
+        <source>Output:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
