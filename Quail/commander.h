f33ac5e295eac924feaed83ccdb684db32603a8b
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2023-02-14 10:03:37 -0800

    add copyright notices and remove Tailscale class (unused)

--- Quail/commander.h
+++ Quail/commander.h
@@ -1,17 +1,30 @@
+/*
+Copyright (C) 2022 Ruby-Dragon
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef COMMANDER_H
 #define COMMANDER_H
 
 #include <QObject>
 #include <QProcess>
 #include <QTimer>
-#include "tailscale.h"
 
 class Commander : public QObject
 {
     Q_OBJECT
 private:
     QString ExitNodeName;
-    Tailscale tailscale;
+    QProcess tailscale;
 
     QString LastOperationText;
     QTimer TimeoutTimer;
--- Quail/tailscale.cpp
+++
@@ -1,7 +0,0 @@
-#include "tailscale.h"
-
-//honestly does not need its own class, but may be useful in the future?
-Tailscale::Tailscale()
-{
-
-}
